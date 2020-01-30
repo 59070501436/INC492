c559f99df2391f112ae345dff201b95dc9f25587
punyapat <punyapat.areerob@mail.kmutt.ac.th>
2020-01-30 10:26:30 +0700

    Add W03-ex1

---
+++ W3/INC492/ex1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace INC492
+{
+    class Rectangle
+    {
+        public double length;
+        public double width;
+
+        public double GetAree()
+        {
+            return length * width;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("length:{0}", length);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Aree:{0}", GetAree());
+        }
+    }
+}
--- W3/INC492/main.cs
+++ W3/INC492/main.cs
@@ -6,7 +6,13 @@ namespace INC492
     {
         public static void Main(string[] args)
         {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
