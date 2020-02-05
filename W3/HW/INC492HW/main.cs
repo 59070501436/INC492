6e7bb0b3a52df89869c2f21188f679d5474da695
punyapat <punyapat.areerob@mail.kmutt.ac.th>
2020-02-05 16:05:49 +0700

    ADD HW week 03

---
+++ W3/HW/INC492HW/main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace INC492HW
+{
+    class INC492HW
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Day: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter months: ");
+            int months = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            DateTime timenow = new DateTime(year, months, day);
+            DateTime dayofyear = new DateTime(year, 12, 31);
+            Console.WriteLine("Dayinput= {0:d}:", timenow);
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("Leap Year!");
+                
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap Year!");
+            }
+            Console.WriteLine("How many day {0}", timenow.DayOfYear);
+            Console.WriteLine("Ordered date = {0}:", dayofyear.DayOfYear);
+           
+
+        }
+    }
+}
